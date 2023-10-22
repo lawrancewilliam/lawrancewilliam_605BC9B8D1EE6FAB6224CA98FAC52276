c3c98493697cdf220c86ce968b6df8cca7ba7528
lawrancewilliam <144717564+lawrancewilliam@users.noreply.github.com>
2023-10-22 15:31:48 +0530

    Add files via upload

--- ex.2.py
+++ ex.2.py
@@ -1,18 +1,21 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+# Example usage:
+student1 = Student("iron_man", "001", 3.9)
+student2 = Student("captain_america", "002", 3.2)
+student3 = Student("spider_man", "003", 3.5)
 
+students = [student1, student2, student3]
 
-batsman1 = Batsman()
-bowler1 = Bowler()
+sorted_students = sort_students(students)
 
-batsman1.play()
-bowler1.play()
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
